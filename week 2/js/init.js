254ecc26d48bbb652f10ef67fa41a104ecea45ea
bittertang <ivyjiatang@g.ucla.edu>
2022-04-11 22:38:22 -0700

    Finishing Week 2

--- week 2/js/init.js
+++ week 2/js/init.js
@@ -1,16 +1,27 @@
-// original code
-const map = L.map('the_map').setView([34.0709, -118.444], 5);
+// JavaScript const variable declaration
+const map = L.map('the_map').setView([26, 30], 2); 
 
+// Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}).addTo(map); 
 
-// adding markers
-let work = L.marker([34.0709, -118.444]).addTo(map)
-        .bindPopup('Where I work on campus')
 
-let home = L.marker([37.7409, -122.484]).addTo(map)
-        .bindPopup('Family home in San Francisco')
+function my_first_function(){
+    console.log('hi from function')
+}
 
-let random = L.marker([39.7409, -122.484]).addTo(map)
-        .bindPopup('Third Point')
+my_first_function()
+
+
+function add_marker(lat,lng,popup){
+    L.circleMarker([lat, lng]).addTo(map) 
+    .bindPopup(popup)
+}
+
+add_marker(37.804363, -122.271111, 'Oakland, California')
+add_marker(22.396427, 114.109497, 'Hong Kong')
+add_marker(21.027763, 105.834160, 'Hanoi, Vietnam')
+add_marker(51.507351, -0.127758, 'London, England')
+add_marker(49.282730, -123.120735, 'Vancouver, Canada')
+
